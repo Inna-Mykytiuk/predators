0023d5495a46004c1a864ed4fbfb3cc86c1ea148
Inna-Mykytiuk <Inna-Mykytiuk@users.noreply.github.com>
2023-12-02 20:30:36 +0000

    deploy: d74cf71e33b24ca3075b75ecbbe5a8bdca3cb916

---
+++ assets/Home-b4e13a59.js
@@ -0,0 +1,113 @@
+import{j as e}from"./index-1bd0c851.js";import{n,k as s,C as a}from"./index.esm-409dd515.js";import{N as r,a as o,b as x,c as t,L as i,W as h,H as d,d as c,S as p,T as l,e as m,f as g,V as f}from"./Navigation.styled-d05037ac.js";const j="/predators/assets/predatorHero-b6a17c78.png",w=n.div`
+  display: flex;
+  background:
+  /* linear-gradient(
+      to right,
+      rgba(47, 48, 58, 0.1),
+      rgba(47, 48, 58, 0.5)
+    ), */ url(${j});
+
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
+`,u=n.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+
+  gap: 10px;
+  width: 100%;
+  height: 100%;
+  margin-right: auto;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    gap: 20px;
+    width: 500px;
+    align-items: center;
+  }
+  @media (min-width: 1440px) {
+    gap: 30px;
+    width: 800px;
+    align-items: center;
+  }
+`,k=n.h1`
+  margin: 0;
+  text-align: center;
+  text-transform: uppercase;
+  color: #a40007;
+  font-family: "Arvo", sans-serif;
+
+  font-weight: bold;
+  font-size: 42px;
+  line-height: 1.6;
+  text-shadow: 1px 1px 2px #181717;
+
+  @media (min-width: 768px) {
+    font-size: 58px;
+    line-height: 1.4;
+  }
+  @media (min-width: 1440px) {
+    font-size: 120px;
+    text-transform: uppercase;
+    font-weight: bold;
+    line-height: 1.2;
+  }
+`,v=n.h2`
+  color: #770409;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.2;
+  max-width: 450px;
+  text-align: center;
+  margin: 0 auto;
+  text-shadow: 1px 1px 2px #181717;
+  margin-top: 20px;
+
+  @media (min-width: 768px) {
+    max-width: 550px;
+    font-size: 18px;
+    line-height: 1.6;
+  }
+  @media (min-width: 1440px) {
+    font-size: 20px;
+    line-height: 1.6;
+  }
+`,b=s`
+  0% {
+    opacity: 0.5;
+    text-shadow: 2px 2px 10px #a40007;
+  }
+  100% {
+    opacity: 1;
+    text-shadow: 2px 2px 20px #a40007;
+  }
+`,y=n.div`
+  font-style: bold;
+  font-size: 24px;
+  text-transform: uppercase;
+  color: transparent;
+  -webkit-text-stroke: #df8080;
+  -webkit-text-stroke-width: 1px;
+  text-shadow: 2px 2px 10px $blue;
+  transition: all 0.5s ease-in-out;
+  text-align: center;
+  letter-spacing: 0.2em;
+  animation: ${b} 0.5s ease-in-out infinite alternate;
+
+  &:hover {
+    color: #fff;
+  }
+
+  /* @media (min-width: 768px) {
+    font-size: 32px;
+    line-height: 1.6;
+  }
+  @media (min-width: 1440px) {
+    font-size: 40px;
+    line-height: 1.6;
+  } */
+`,z=()=>e.jsx(r,{children:e.jsx(o,{children:e.jsxs(x,{children:[e.jsx(t,{children:e.jsx(i,{to:"/wasp",children:e.jsx(h,{})})}),e.jsx(t,{children:e.jsx(i,{to:"/hawk",children:e.jsx(d,{})})}),e.jsx(t,{children:e.jsx(i,{to:"/wolf",children:e.jsx(c,{})})}),e.jsx(t,{children:e.jsx(i,{to:"/snake",children:e.jsx(p,{})})}),e.jsx(t,{children:e.jsx(i,{to:"/tiger",children:e.jsx(l,{})})}),e.jsx(t,{children:e.jsx(i,{to:"/shark",children:e.jsx(m,{})})}),e.jsx(t,{children:e.jsx(i,{to:"/human",children:e.jsx(g,{})})}),e.jsx(t,{children:e.jsx(i,{to:"/virus",children:e.jsx(f,{})})})]})})}),I=()=>e.jsx(a,{children:e.jsxs(u,{children:[e.jsxs("div",{children:[e.jsx(k,{children:"Predators"}),e.jsx(y,{children:"-- of the Earth --"}),e.jsx(v,{children:"Welcome to the captivating realm of Earth's most formidable and fascinating predators. In a world where each species unveils its unique prowess."})]}),e.jsx(z,{})]})}),T=()=>e.jsx(w,{children:e.jsx(I,{})});export{T as default};
---
+++ assets/Wasp-887ba1ea.js
@@ -0,0 +1,182 @@
+import{r as o,j as i}from"./index-1bd0c851.js";import{n as t,k as r,C as p}from"./index.esm-409dd515.js";import{R as l,g as x,h as d,B as c,G as a,i as h,j as m}from"./Navigation.styled-d05037ac.js";const f="/predators/assets/waspHero-9e1391d1.png",g=t.div`
+  display: flex;
+  background: url(${f});
+
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  min-height: 100vh;
+`,w=t.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-end;
+  padding: 30px 0 60px;
+  width: 100%;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    gap: 20px;
+  }
+  @media (min-width: 1440px) {
+    padding: 50px 0 50px;
+    gap: 30px;
+    width: 100%;
+  }
+`,u=t.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  width: 100%;
+  height: 100%;
+  margin-left: auto;
+
+  @media (min-width: 768px) {
+    align-items: flex-end;
+  }
+  @media (min-width: 1440px) {
+    align-items: flex-end;
+  }
+`,b=t.h1`
+  margin: 0;
+  text-align: center;
+  text-transform: uppercase;
+  color: #ff6000;
+  font-family: "Antonio", sans-serif;
+
+  font-weight: bold;
+  font-size: 48px;
+  line-height: 1.6;
+  text-shadow: 1px 1px 2px #181717;
+  margin-bottom: 20px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 30px;
+    font-size: 80px;
+    line-height: 1.4;
+  }
+  @media (min-width: 1440px) {
+    margin-bottom: 40px;
+    font-size: 120px;
+    text-transform: uppercase;
+    font-weight: bold;
+    line-height: 1.2;
+  }
+`,j=r`
+  0% {
+    background-position: -500%;
+  }
+  100% {
+    background-position: 500%;
+  }
+`,k=t.h2`
+  font-family: sans-serif;
+  text-transform: uppercase;
+  font-size: 14px;
+  letter-spacing: 4px;
+  line-height: 1.2;
+  background: linear-gradient(90deg, #ff6000, #a40007, #ff6000);
+  background-repeat: no-repeat;
+  background-size: 80%;
+  animation: animate 3s linear infinite;
+  background-clip: text;
+  -webkit-text-fill-color: rgba(255, 255, 255, 0);
+  animation: ${j} 4s linear infinite;
+  margin-bottom: 20px;
+
+  @media (min-width: 768px) {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+  @media (min-width: 1440px) {
+    font-size: 18px;
+  }
+`,y=t.p`
+  color: #99360f;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.2;
+  max-width: 350px;
+  text-align: center;
+  margin-left: auto;
+  text-shadow: 1px 1px 2px #181717;
+  text-align: start;
+
+  @media (min-width: 768px) {
+    text-align: end;
+    font-size: 16px;
+    max-width: 450px;
+    line-height: 1.5;
+  }
+  @media (min-width: 1440px) {
+    font-size: 18px;
+    max-width: 500px;
+  }
+`,v=t.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  margin-top: 10px;
+
+  @media (min-width: 768px) {
+    align-items: flex-end;
+    margin-top: 20px;
+  }
+`,z=t.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding: 10px 0 20px 0;
+`,W=t.h3`
+  font-size: 22px;
+  font-weight: bold;
+  color: #ff6000;
+  font-family: "Antonio", sans-serif;
+`,R=t.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 0 10px;
+  max-width: 400px;
+  opacity: 0;
+
+  color: #fff;
+  -webkit-backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
+  backdrop-filter: blur(10px);
+  transform: translateY(0);
+  opacity: ${({isVisible:e})=>e?1:0};
+  transform: ${({isVisible:e})=>e?"translateY(0)":"translateY(-20px)"};
+  height: ${({isVisible:e})=>e?"100%":"30px"};
+  overflow: ${({isVisible:e})=>e?"visible":"hidden"};
+  transition: all 0.5s;
+
+  @media (min-width: 768px) {
+    font-size: 28px;
+    text-align: end;
+  }
+`,n=t.li`
+  color: #99360f;
+  font-family: "Antonio", sans-serif;
+  font-size: 12px;
+  font-weight: normal;
+  text-align: start;
+
+  text-shadow: rgba(255, 255, 255, 0.1) -1px -1px 1px,
+    rgba(0, 0, 0, 0.5) 1px 1px 1px;
+  text-shadow: 2px 2px 5px #1d1e26;
+
+  @media (min-width: 768px) {
+    font-size: 16px;
+    text-align: end;
+  }
+  @media (min-width: 1440px) {
+    font-size: 16px;
+  }
+`,S=()=>{const[e,s]=o.useState(!1);return i.jsx(g,{children:i.jsx(w,{children:i.jsx(p,{children:i.jsxs(u,{children:[i.jsx(b,{children:"Wasp"}),i.jsx(k,{children:"Nature's Architects and Stingers"}),i.jsx(y,{children:"Enter the intricate world of wasps, where social structures, stingers, and vital ecological roles converge in a dance of precision and purpose."}),i.jsxs(v,{children:[i.jsxs(z,{children:[i.jsx(W,{children:"Danger rating 5/10"}),i.jsx(l,{onClick:()=>s(!e),children:e?i.jsx(x,{}):i.jsx(d,{})})]}),i.jsxs(R,{isVisible:e,children:[i.jsx(n,{children:"Some wasps are social, forming intricate colonies with specialized roles for individuals."}),i.jsx(n,{children:"Social wasps, like yellow jackets, can form large colonies with hierarchical structures."}),i.jsx(n,{children:"Wasps possess a venomous sting used for hunting and defense against predators."}),i.jsx(n,{children:"Certain wasp species exhibit parasitic behavior, laying eggs on or inside other insects."}),i.jsx(n,{children:"Pollination is another essential role of certain wasp species, contributing to plant diversity."}),i.jsx(n,{children:"Wasps communicate through chemical signals, coordinating activities within their colonies."}),i.jsx(n,{children:"Mimicry is a common defense mechanism among wasps, imitating the appearance of more dangerous species."})]})]}),i.jsxs(c,{children:[i.jsx(a,{to:"/",children:i.jsx(h,{})}),i.jsx(a,{to:"/hawk",children:i.jsx(m,{})})]})]})})})})},$=()=>i.jsx(S,{});export{$ as default};
